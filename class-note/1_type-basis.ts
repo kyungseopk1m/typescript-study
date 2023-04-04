ee4f19d9b5d06d7ea87858d7eaaed836a1bd5190
kyungseopk1m <kks0919@kakao.com>
2023-04-04 14:58:43 +0900

    기본타입 - 듀플, 객체, 진위값

--- class-note/1_type-basis.ts
+++ class-note/1_type-basis.ts
@@ -12,4 +12,19 @@ let arr: Array<Number> = [1,2,3];
 let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', '10'];
 let items: number[] = [1,2,3];
 
-//
+// TS 튜플
+let address: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 100
+// };
+let person: { name: string, age: number} = {
+    name: 'thor',
+    age: 1000
+}
+
+// TS 진위값
+let show: boolean = true;
