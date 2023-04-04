3f827c612dc371fee5183d863f8d8f58d2a0d12c
kyungseopk1m <kks0919@kakao.com>
2023-04-04 17:44:56 +0900

    인터페이스 딕셔너리 패턴

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -35,3 +35,21 @@ interface StringArray {
 }
 
 var arr: StringArray = ['a', 'b', 'c'];
+// arr[0] = 10;
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp;
+}
+
+var obj: StringRegexDictionary = {
+    // sth: /abc/,
+    cssFile: /\.css$/,
+    jsFile: /\.js$/,
+}
+
+// obj['cssFile'] = 'a'
+
+Object.keys(obj).forEach(function(value) {
+
+}) 
