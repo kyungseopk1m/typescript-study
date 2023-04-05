9b7fd66d79bd83acc4b0c52c2dd978dd803061e9
kyungseopk1m <kks0919@kakao.com>
2023-04-05 12:21:20 +0900

    keyof로 제네릭의 타입 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -12,16 +12,16 @@
 // }
 // logText<string>('하이');
 
-function logText(text: string) {
-    console.log(text);
-    // text.split('').reverse().join('');
-    return text;
-}
+// function logText(text: string) {
+//     console.log(text);
+//     // text.split('').reverse().join('');
+//     return text;
+// }
 
-function logNumber(num: number) {
-    console.log(num);
-    return num;
-}
+// function logNumber(num: number) {
+//     console.log(num);
+//     return num;
+// }
 
 // function logText(text: string | number) {
 //   console.log(text);
@@ -58,3 +58,38 @@ interface Dropdown<T> {
     selected: boolean;
 }
 const obj: Dropdown<number> = { value: 'abc', selected: false };
+
+// 제네릭의 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach(function(text) {
+//         console.log(text);
+//     });
+//     return text; 
+// }
+// logTextLength<string>(['hi', ' abc ']);
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+    text.length;
+    return text;
+}
+logTextLength(10);
+logTextLength({ length: 10 });
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+    return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption('name');
