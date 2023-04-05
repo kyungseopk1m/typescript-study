4a6a1ef38d9b48fcb47837a434327f711faea5e4
kyungseopk1m <kks0919@kakao.com>
2023-04-05 16:35:19 +0900

    타입 추론

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -9,11 +9,31 @@ function getB(b = 10) {
 10 + '10' // 1010
 
 // 타입 추론 기본 2
+// interface Dropdown<T> {
+//     value: T;
+//     title: string;
+// }
+// var shoppingItem: Dropdown<string> = {
+//     value: 'abc',
+//     title: 'hello'
+// }
+
+// 타입 추론 기본 3
 interface Dropdown<T> {
     value: T;
     title: string;
 }
-var shoppingItem: Dropdown<string> = {
-    value: 'abc',
-    title: 'hello'
+interface DetailedDropdown<K> extends Dropdown<K> {
+    description: string;
+    tag: K;
 }
+
+var detailedItem: DetailedDropdown<string> = {
+    title: 'abc',
+    description: 'ab',
+    value: 'a',
+    tag: 'a'
+}
+
+// Best Common Type
+var arr = [1,2,true, true, 'a'];
