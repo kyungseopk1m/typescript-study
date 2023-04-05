104479a0dee9c3a7bf589cb3ae90691db9f87e76
kyungseopk1m <kks0919@kakao.com>
2023-04-05 09:42:13 +0900

    유니온 타입

---
+++ class-note/5_operator.ts
@@ -0,0 +1,35 @@
+// function logMessage(value: any) {
+//     console.log(value);
+// }
+// logMessage('hello');
+// logMessage(100);
+
+var seho: string | number | boolean;
+function logMessage(value: string | number) {
+    if (typeof value === 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value === 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number');
+}
+logMessage('hello');
+logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+    someone.name;
+    // 유니온 타입 : 보장된(공통된) 속성만 제공!
+    // someone.skill
+    // someone.age
+}
